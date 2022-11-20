984898102d5eccff13f7e7fcd8068b19931f3352
emanfeah <emanfeah1@gmail.com>
2022-11-20 21:34:41 +0300

    change

--- Terraform/modules/aws-elasticbeanstalk-cloudfront/variables.tf
+++ Terraform/modules/aws-elasticbeanstalk-cloudfront/variables.tf
@@ -1,8 +1,8 @@
 variable "eb-app-name" {
-  default = "Elasticbeanstalk-app"
+  default = "app"
 }
 variable "eb-env-name" {
-  default = "Elasticbeanstalk-env"
+  default = "App-env"
 }
 variable "eb-solution-stack-name" {
   default = "64bit Amazon Linux 2 v4.2.18 running Tomcat 8.5 Corretto 11"
